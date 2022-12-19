e76d4f04c0a9cd4aad4883fd17943b93416f8e78
songquanquan <2978908628@qq.com>
2022-12-19 22:48:05 +0800

    数据结构实验

--- 3_Bittree.cpp
+++ 3_Bittree.cpp
@@ -1,5 +1,7 @@
 
 #include<iostream>
+#include <queue>
+
 //二叉树
 
 #define MAXSIZE 100
@@ -12,27 +14,53 @@ typedef int Status;
 
 using namespace std;
 
-template<class T>
-struct BiNode {
-    T data;    // 数据域
-    BiNode<T> *lchild, *rchild;    // 左右指针域
-};
+typedef struct node {
+    char data;    // 数据域
+    node *lchild, *rchild;    // 左右指针域
+} BNode, *BTree;
 
-template<class T>
 class Bitree {
+
 public:
     Bitree() { root = Creat(root); }
 
-    ~Bitree() { Release(root); }
+    ~Bitree() { Release(); }
+
+    void PreOlder() {
+        PreOrderTraverse(this->root);
+        cout << endl;
+    }
+
+    void InOlder() {
+        InOrderTraverse(this->root);
+        cout << endl;
+    }
+
+    void PostOlder() {
+        PostOrderTraverse(this->root);
+        cout << endl;
+    }
+
+    void BinOlder() {
+        BinaryOrder(this->root);
+        cout << endl;
+    }
+
+    void Release() {
+        re(this->root);
+    }
+
+private:
+    BTree root;
 
     //（1）初始化二叉链表：理解扩展二叉树及其前序遍历序列；
-    BiNode<T> *Creat(BiNode<T> *bt) {
-        T ch;
+    BTree Creat(BTree bt) {
+        char ch;
         cin >> ch;  // 输入结点的数据信息，假设为字符
         if (ch == '#')      // 建立一棵空树
             bt = NULL;
         else {
-            bt = new BiNode<T>; // 生成一个结点，数据域为ch
+            bt = new node; // 生成一个结点，数据域为ch
             bt->data = ch;
             bt->lchild = Creat(bt->lchild); // 递归建立左子树
             bt->rchild = Creat(bt->rchild); // 递归建立右子树
@@ -41,9 +69,10 @@ public:
     }
 
 //（2）前序遍历二叉链表（递归实现）；
-    Status PreOrderTraverse(BiNode<T> bt) {
+
+    Status PreOrderTraverse(BTree bt) {
         if (bt) {
-            cout << bt->data;
+            cout << bt->data ;
             PreOrderTraverse(bt->lchild);
             PreOrderTraverse(bt->rchild);
         }
@@ -51,7 +80,7 @@ public:
     }
 
 //（3）中序遍历二叉链表（递归实现）；
-    Status InOrderTraverse(BiNode<T> bt) {
+    Status InOrderTraverse(BTree bt) {
         if (bt) {
             InOrderTraverse(bt->lchild);
             cout << bt->data;
@@ -61,7 +90,7 @@ public:
     }
 
 //（4）后序遍历二叉链表（递归实现）；
-    Status PostOrderTraverse(BiNode<T> bt) {
+    Status PostOrderTraverse(BTree bt) {
         if (bt) {
             PostOrderTraverse(bt->lchild);
             PostOrderTraverse(bt->rchild);
@@ -70,21 +99,48 @@ public:
         return OK;
     }
 
-//（5）层序遍历二叉链表；
+//（5）层序遍历二叉链表
+
+    void BinaryOrder(BTree root) {
+        queue<BTree> q;
+        //树为空，直接返回
+        if (root == nullptr) {
+            cout << "空" << endl;
+            return;
+        }
+        //先将根节点入队
+        q.push(root);
+        while (!q.empty()) {
+            //出队保存队头并访问
+            BTree front = q.front();
+            cout << front->data;
+            q.pop();
+            //将出队结点的左子树根入队
+            if (front->lchild)
+                q.push(front->lchild);
+            //将出队结点的右子树根入队
+            if (front->rchild)
+                q.push(front->rchild);
+        }
+    }
 
 //（6）销毁二叉链表（递归实现）。
-    void Release(BiNode<T> &bt) {
+    void re(BTree bt) {
         if (bt != nullptr) {
-            Release(bt->lchild);    // 释放左子树
-            Release(bt->rchild);    // 释放右子树
+            re(bt->lchild);    // 释放左子树
+            re(bt->rchild);    // 释放右子树
             delete bt;  // 释放根节点
         }
     }
 
 
-    BiNode<T> *root;
 };
-int main()
-{
-Bitree<char>* A=new Bitree<char>;
+
+int main() {
+    Bitree a;//ABD##E##CF##G##
+    a.InOlder();//DBEAFCG
+    a.PostOlder();//DEBFGCA
+    a.PreOlder();//ABDECFG
+    a.BinOlder();//ABCDEFG
+    a.Release();
 }
