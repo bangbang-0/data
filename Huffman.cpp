d824ff3367aa7c52bf26a2cb9c4213591d206745
songquanquan <2978908628@qq.com>
2022-12-21 14:55:45 +0800

    数据结构实验

--- Huffman.cpp
+++ Huffman.cpp
@@ -23,7 +23,7 @@ int main() {
         for (auto x: sts) {
             cout << x.first << ":" << x.second << "  ";
         }
-        cout<<endl;
+        cout << endl;
         int len = sts.size();
         Node T = new node[2 * len];
         for (int i = 0; i < 2 * len; ++i) {
@@ -40,24 +40,24 @@ int main() {
             int min = INT_MAX;
             int loc;
             for (int i = 1; i < j - 1; i++) {
-                if (T[i].weight < min&&T[i].parent==0) {
+                if (T[i].weight < min && T[i].parent == 0) {
                     loc = i;
                     min = T[i].weight;
-                    s1=i;
+                    s1 = i;
                 }
             }
-            int temp=T[loc].weight,tep=loc;//暂存,再找第二个最小值
-            T[loc].weight= INT_MAX;
+            int temp = T[loc].weight, tep = loc;//暂存,再找第二个最小值
+            T[loc].weight = INT_MAX;
             min = INT_MAX;
             for (int i = 1; i <= j - 1; i++) {
-                if (T[i].weight < min&&T[i].parent==0) {
+                if (T[i].weight < min && T[i].parent == 0) {
                     loc = i;
                     min = T[i].weight;
-                    s2=i;
+                    s2 = i;
                 }
             }
-            T[tep].weight=temp;
-           //
+            T[tep].weight = temp;
+            //
             T[s1].parent = j;
             T[s2].parent = j;
             T[j].lchild = s1;
@@ -66,9 +66,54 @@ int main() {
 
         }
         for (int i = 1; i < 2 * len; ++i) {
-            cout<<i<<" " << T[i].weight << " " << T[i].parent << " " << T[i].lchild << " " << T[i].rchild << endl;
+            cout << i << " " << T[i].weight << " " << T[i].parent << " " << T[i].lchild << " " << T[i].rchild << endl;
         }
 
+
+        //====编码====
+        int ii = 1;
+        map<char, string> str;
+        for (auto x: sts) {
+            cout << x.first << ":";
+            node temp = T[ii];
+            int nod = ii;
+            string na = "";
+            do {
+                if (T[temp.parent].lchild == nod)na += "0";
+                if (T[temp.parent].rchild == nod)na += "1";
+                nod = temp.parent;
+                temp = T[temp.parent];
+            } while (temp.parent != 0);
+            ii++;
+            reverse(na.begin(), na.end());
+            cout << na << " ";
+            str.insert(pair<char, string>(x.first, na));
+        }
+        string po = "";
+        for (auto x: ques) {
+            for (auto y: str) {
+                if (x == y.first) {
+                    po += y.second;
+                    break;
+                }
+            }
+        }
+        cout << endl << po << endl;
+
+
+        // ====解码====
+        string jie = "", su = "";
+        for (auto x: po) {
+            jie += x;
+            for (auto y: str) {
+                if (jie == y.second) {
+                    su += y.first;
+                    jie="";
+                    break;
+                }
+            }
+        }
+        cout<<su;
         cin >> ques;
     }
 }
